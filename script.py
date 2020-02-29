d6763d828715068bf0d08bc152ec82ac2838f636
carol80 <carolseb80@gmail.com>
2020-02-29 18:28:13 +0530

    firebase

---
+++ script.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import smtplib
+
+SenderAddress = "yobro9594246827@gmail.com"
+password = "thetimesofindia"
+
+e = pd.read_excel("Email.xlsx")
+emails = e['Emails'].values
+email = 'abdulshahlatiiffkhamura@gmail.com'
+print(emails,email)
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login(SenderAddress, password)
+msg = "Chal raha hai bantai!!!"
+subject = "Sabooo pachas!!"
+body = "Subject: {}\n\n{}".format(subject,msg)
+# for email in emails:
+#     server.sendmail(SenderAddress, email, body)
+# server.quit()
+for i in range(0,5):
+    server.sendmail(SenderAddress, email, body)
