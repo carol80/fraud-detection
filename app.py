3f1545be5aaa6f03f166088e9db78287e54e790b
ejson03 <elvisejsondsouza57@gmail.com>
2020-02-29 16:50:49 +0530

    Flask server serving email model

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,url_for, request, jsonify
+import pandas as pd 
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.multiclass import *
+from sklearn.svm import *
+import json
+
+def get_predict(text):
+	try:
+		if len(text) > 0:
+			vectorize_message = vectorizer.transform([text])
+			predict = model.predict(vectorize_message)[0]
+		else:
+			predict = 2
+	except BaseException as inst:
+		error = str(type(inst).__name__) + ' ' + str(inst)
+	
+	return predict
+
+app = Flask(__name__)
+
+
+# train model
+model = pickle.load(open('models/PassiveAggressiveClassifier_with_TfidfVectorizer.pkl', 'rb'))
+vectorizer = pickle.load(open('vectors/TfidfVectorizer.pkl', 'rb'))
+
+@app.route('/', methods=['POST'])
+def main():
+	data = request.get_json()
+	body = data['body']
+	predict = get_predict(body)
+	response = app.response_class(
+        response=json.dumps(predict),
+        status=200,
+        mimetype='application/json'
+    )
+	return response
+
+if __name__ == '__main__':
+	app.run(debug=True, use_reloader=True, threaded=True)
